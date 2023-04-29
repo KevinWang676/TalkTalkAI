d4bb28ccf947abf6da1d48c601fb33066c66d745
Kevin Wang <126712357+KevinWang676@users.noreply.github.com>
2023-04-28 21:28:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import gradio as gr
 import os
 
-def inference(text,audio):
-  os.system('tts --text "'+text+'" --model_name tts_models/multilingual/multi-dataset/your_tts --speaker_wav '+audio+' --language_idx "en"')
-  return "tts_output.wav"
-  
+stable_diffusion = gr.Blocks.load(name="spaces/runwayml/stable-diffusion-v1-5")
+
+def get_images(prompt):
+    gallery_dir = stable_diffusion(prompt, fn_index=2)
+    sd_output = [os.path.join(gallery_dir, image) for image in os.listdir(gallery_dir)]
+    return sd_output
+
+
 with gr.Blocks() as demo:
     
-    with gr.Tab("声音克隆-极速版"):
+    with gr.Tab("AI作画-极速版"):
 
         gr.Markdown('''# <center>🥳 滔滔AI 🎶</center>
                 ### <center>🥰 - 滔滔AI，让有爱的AI滔滔不绝</center>
@@ -16,13 +20,14 @@ with gr.Blocks() as demo:
         ''')
 
         with gr.Row():
-            inp1 = gr.Textbox(label="请填写想要进行声音克隆的文本(英文)", placeholder="Hello world", lines=3)
-            inp2 = gr.Audio(label="请上传一段您喜欢的语音", type="filepath")
+            inp1 = gr.Textbox(label="请提供关键词(英文)", value="A large cabin on top of a sunny mountain in the style of Dreamworks, artstation", lines=3)
+            btn = gr.Button("开始绘制您的专属作品吧")
         with gr.Row():
-            btn = gr.Button("开始合成您的专属语音吧")
-            out1 = gr.Audio(label="为您合成的专属语音", type="filepath")
-
-        btn.click(inference, [inp1, inp2], [out1])
+            out1 = gr.Gallery(
+                label="为您绘制的专属作品", show_label=True, elem_id="gallery"
+            )
+        
+        btn.click(get_images, [inp1], [out1])
 
 
         gr.HTML('''
@@ -53,10 +58,10 @@ with gr.Blocks() as demo:
             """ 
             ## 😄 - 更多精彩尽在滔滔AI
             
-            ## 🖌️ [Stable Diffusion](https://kevinwang676-stable-diffusion-for-all.hf.space)：AI画家，为您执笔作画
+            ## 🖌️ [Stable Diffusion全家桶](https://kevinwang676-stable-diffusion-for-all.hf.space)：AI画家，为您执笔作画
             ## 🎶 [Sovits](https://kevinwang676-voice-cloning-for-bilibili.hf.space)：AI歌手，为您一展歌喉
             
-            ## 🤖 [声音克隆-高级版](https://kevinwang676-voice-cloning-demo.hf.space)：AI拟声，为您妙语连珠
+            ## 🤖 [声音克隆](https://kevinwang676-voice-cloning-demo.hf.space)：AI拟声，为您妙语连珠
         """
         )
     
